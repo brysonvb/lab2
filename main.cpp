c1172cbd8b9a59b1607bebc4ba2c216c93fc52b1
brysonvb <57158207+brysonvb@users.noreply.github.com>
2020-08-23 18:24:08 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 int hello1Function(string name1);
 int hello2Function(string name2);
 
+// Authors: Bryson Bisuna and Parsa Bidram
+// Description: Hello World in C++
+
 int main()
 {
 	string name1 = "your name1";
