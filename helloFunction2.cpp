604ce3322b6d5f506b8e4ea5ee0dc253abb02408
brysonvb <57158207+brysonvb@users.noreply.github.com>
2020-08-26 18:03:15 -0700

    Update helloFunction2.cpp

--- helloFunction2.cpp
+++ helloFunction2.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 
+// Bryson Bisuna and Parsa Bidram
+// Hello World in C++
+
 int helloFunction2(string name2)
 {
   cout << "hello World " << name2 << endl;
